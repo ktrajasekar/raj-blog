fb3469f8cbff1ea7e6b28e37cf75b58701e3420f
Rajasekar T <kt.rajasekar@gmail.com>
2023-07-23 11:30:53 +0530

    Initial Commit

---
+++ app/blog/blog.js
@@ -0,0 +1,32 @@
+export default function Blog() {
+  const post = [
+    {
+      id: 1,
+      title: "How to learn react in 10 days",
+      description: "Desc text",
+    },
+    {
+      id: 2,
+      title: "How to learn react in 10 days",
+      description: "Desc text",
+    },
+    {
+      id: 3,
+      title: "How to learn react in 10 days",
+      description: "Desc text",
+    },
+    {
+      id: 4,
+      title: "How to learn react in 10 days",
+      description: "Desc text",
+    },
+  ];
+
+  return (
+    <div>
+      <div className="blog-post-container">
+        {post && post.map((item) => <div>{item.title}</div>)}
+      </div>
+    </div>
+  );
+}
