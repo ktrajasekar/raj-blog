4fae652b5d896745a728d89f4cb6edabdd465e96
Rajasekar T <kt.rajasekar@gmail.com>
2023-07-23 23:03:51 +0530

    Nav added

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -1,3 +1,49 @@
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 export default function Header() {
-  return <div className="header-bg">Header</div>;
+  const router = useRouter();
+  const currentPath = router.pathname;
+  return (
+    <header className="site-header header-bg">
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <h2>Rajasekar Thangavel</h2>
+          </div>
+          <div className="col-8">
+            <nav className="site-navigation">
+              <ul>
+                <li>
+                  <Link
+                    href="/"
+                    className={currentPath === "/" ? "active" : ""}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/about"
+                    className={currentPath === "/about" ? "active" : ""}
+                  >
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/blog"
+                    className={currentPath === "/blog" ? "active" : ""}
+                  >
+                    Blog
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
